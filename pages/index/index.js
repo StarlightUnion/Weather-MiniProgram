f4bfab5c3b53372c34e47b4324813a58125dbf0e
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-12-24 22:55:56 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,40 @@
+Page({
+  data: {
+    region: ['浙江省', '杭州市', '萧山区'],
+    now: ''
+  },
+  changeRegion: function (e) {
+    this.setData({
+      region: e.detail.value,
+    });
+    this.getweather();
+  },
+  getweather: function () {
+    wx.showLoading({ title: '获取数据中', });
+    let _this = this;
+
+    wx.request({
+      url: 'https://free-api.heweather.net/s6/weather/now',
+      data: {
+        location: _this.data.region[1],
+        key: 'd1f0be233dbc4a649c4918e6b1535c83'
+      },
+      success: function (res) {
+        if (res.data.HeWeather6[0].status === "ok") {
+          console.log(res.data);
+          _this.setData({now: res.data.HeWeather6[0].now});
+          wx.hideLoading();
+        } else {
+          wx.showToast({
+            title: '数据获取失败',
+            icon: 'success',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+  onLoad: function (options) {
+    this.getweather();
+  }
+})
